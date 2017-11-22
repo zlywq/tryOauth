6caafa329c57952c9bda66c8a158f297a5ead168
zlywk <zhanglianyuwork@qq.com>
2017-11-22 19:58:43 +0800

    modified little for nginx support

--- sprbtOauthLogin/src/main/java/g1/oauthlogin/sprsec/MyAuthenticationEntryPoint.java
+++ sprbtOauthLogin/src/main/java/g1/oauthlogin/sprsec/MyAuthenticationEntryPoint.java
@@ -1,12 +1,16 @@
 package g1.oauthlogin.sprsec;
 
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import g1.libcmn.util.*;
+import org.springframework.security.web.util.RedirectUrlBuilder;
+import org.springframework.security.web.util.UrlUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +23,16 @@ import java.util.HashMap;
 public class MyAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
 	
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+	@Value("${myserver.awareReverseProxyUrl.scheme}")
+	String myserver_awareReverseProxyUrl_scheme;
+    @Value("${myserver.awareReverseProxyUrl.serverName}")
+    String myserver_awareReverseProxyUrl_serverName;
+    @Value("${myserver.awareReverseProxyUrl.serverPort}")
+    int myserver_awareReverseProxyUrl_serverPort;
+
+
 
 	public MyAuthenticationEntryPoint(String loginFormUrl) {
 		super(loginFormUrl);
@@ -56,7 +70,39 @@ public class MyAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint
 		}
 	}
 
+	/*
+	这里是为了解决前端有nginx代理时出的一个问题。详细说明见有关nginx的说明部分。
+	 */
+	protected String buildRedirectUrlToLoginPage(HttpServletRequest request,
+												 HttpServletResponse response, AuthenticationException authException) {
+        String url ;
+	    if (StringUtils.isNotEmpty(myserver_awareReverseProxyUrl_serverName)){
+            String loginForm = determineUrlToUseForThisRequest(request, response,authException);
+//            if (UrlUtils.isAbsoluteUrl(loginForm)) {
+//                return loginForm;
+//            }
+            RedirectUrlBuilder urlBuilder = new RedirectUrlBuilder();
+            String scheme = myserver_awareReverseProxyUrl_scheme;
+            if (StringUtils.isEmpty(scheme)){
+                scheme = request.getScheme();
+            }
+            int serverPort = myserver_awareReverseProxyUrl_serverPort;
+            if (serverPort == 0){
+                //serverPort = portResolver.getServerPort(request);
+                request.getServerPort();
+            }
 
+            urlBuilder.setScheme(scheme);
+            urlBuilder.setServerName(myserver_awareReverseProxyUrl_serverName);
+            urlBuilder.setPort(serverPort);
+            urlBuilder.setContextPath(request.getContextPath());
+            urlBuilder.setPathInfo(loginForm);
+            url = urlBuilder.getUrl();
+        }else{
+	        url = super.buildRedirectUrlToLoginPage(request,response,authException);
+        }
+        return url;
+	}
 	
 
 }
